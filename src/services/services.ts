f6d534a3b846c8d6232ee0be5139536e1a805ecc
Vovchik333 <vavan123music@gmail.com>
2024-06-04 01:17:19 +0300

    add http and binance services

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,5 +1,16 @@
 import AuthService from "./auth/auth.service";
+import BinanceService from "./binance/binance.service";
+import HttpApi from "./http/http-api.service";
 
+const httpApi = new HttpApi();
 const authService = new AuthService();
+const binanceService = new BinanceService({
+    apiPath: 'https://api.binance.com',
+    httpApi
+});
 
-export { authService };
+
+export { 
+    authService,
+    binanceService
+};
