b6fc09c9ba71bca36a65761781410e5dff522b8b
Volodymyr <vavan123music@gmail.com>
2024-07-01 23:36:45 +0300

    add table template and icon link components

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,6 +1,7 @@
 export { Button } from './button/Button';
 export { Icon } from './icon/Icon';
 export { IconButton } from './icon-button/IconButton';
+export { IconLink } from './icon-link/IconLink';
 export { InfoBlock } from './info-block/InfoBlock';
 export { Input } from './input/Input';
 export { Modal } from './modal/Modal';
@@ -8,3 +9,4 @@ export { PrivateRoute } from './private-route/PrivateRoute';
 export { PublicRoute } from './public-route/PublicRoute';
 export { Spinner } from './spinner/Spinner';
 export { FormTemplate } from './form-template/FormTemplate';
+export { TableTemplate } from './table-template/TableTemplate';
---
+++ src/components/icon-link/IconLink.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router-dom";
+import { ValueOf } from "../../common/generic/generic";
+import { AppRoute, IconName } from "../../common/enums/enums";
+import { Icon } from "../components";
+import './IconLink.css';
+
+type Props = {
+    className?: string;
+    name: ValueOf<typeof IconName>;
+    to: ValueOf<typeof AppRoute>;
+    children: string;
+    onClick?: React.MouseEventHandler<HTMLAnchorElement>;
+}
+
+const IconLink: React.FC<Props> = ({
+    className,
+    name,
+    to,
+    children,
+    onClick
+}) => {
+    return (
+        <Link className={className} to={to} onClick={onClick}>
+            <Icon className="icon" name={name} />
+            <span className="icon-link__text">{children}</span>
+        </Link>
+    );
+}
+
+export { IconLink };
