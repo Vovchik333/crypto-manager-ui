e3cc7e72efeecb056dd51525cdc10209ba1148a9
Volodymyr <vavan123music@gmail.com>
2024-09-07 21:04:23 +0300

    added a check for the existence of a JWT token at the user

--- src/pages/layout/Layout.tsx
+++ src/pages/layout/Layout.tsx
@@ -1,8 +1,29 @@
 import { Outlet } from "react-router-dom";
 import { Footer } from "./components/footer/Footer";
 import { Header } from "./components/header/Header";
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "@/lib/hooks";
+import { loadCurrentUser } from "@/store/auth/actions";
+import { Spinner } from "@/lib/components";
+import { StorageKey } from "@/lib/enums/storage";
 
 const Layout: React.FC = () => {
+    const dispatch = useAppDispatch();
+    const { user } = useAppSelector(state => state.auth);
+
+    const hasUser = user !== null;
+    const hasToken = Boolean(localStorage.getItem(StorageKey.TOKEN));
+
+    useEffect(() => {
+        if (hasToken) {
+            dispatch(loadCurrentUser());
+        }
+    }, [hasToken]);
+
+    if (hasToken && !hasUser) {
+        return <Spinner />
+    }
+
     return (
         <>
             <Header />
--- src/pages/layout/components/footer/Footer.tsx
+++ src/pages/layout/components/footer/Footer.tsx
@@ -1,11 +1,11 @@
-import './Footer.css';
+import styles from './styles.module.scss';
 
 const Footer: React.FC = () => {
     return (
-        <footer className='footer'>
+        <footer className={styles.footer}>
             <span 
-                className='footer__author-info'
-            >Created by <a className='footer__author-info-link' href="https://github.com/Vovchik333" target='_blank'>Volodymyr Potapenko</a>
+                className={styles['footer__author-info']}
+            >Created by <a className={styles['footer__author-info-link']} href="https://github.com/Vovchik333" target='_blank'>Volodymyr Potapenko</a>
             </span>
         </footer>
     );
