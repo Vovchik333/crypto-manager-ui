18b01ceeb9bbc3bc6a9fa48069f8fb5038ffee7a
Volodymyr <vavan123music@gmail.com>
2024-09-07 21:05:13 +0300

    fix home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,28 +1,26 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../common/enums';
-import { Button } from '../../components/button/Button';
-import assetsHomeImage from '../../assets/images/assets_home.png';
-import './Home.css';
+import { AppRoute } from '@/common/enums';
+import { Button } from '@/lib/components/button/Button';
+import assetsHomeImage from '@/assets/images/assets_home.png';
+import styles from './styles.module.scss';
 
 const Home = () => {
     return (
-        <main className="home-page">
-            <section className='home-page__description-section'>
-                <h1 className='home-page__title'>Meet the most convenient and useful crypto manager</h1>
-                <p className='home-page__description'>
+        <main className={styles['home-page']}>
+            <section className={styles['home-page__description-section']}>
+                <h1 className={styles['home-page__title']}>Meet the most convenient and useful crypto manager</h1>
+                <p className={styles['home-page__description']}>
                     Gain good investment management experience. 
                     The app has many analytical tools that will 
                     help you monitor the state of your crypto portfolio.
                 </p>
-                <Link className='home-page__sign-in-link' to={AppRoute.SIGN_IN}>
-                    <Button 
-                        className='button primary-button'
-                    >Get Started</Button>
+                <Link className={styles['home-page__sign-in-link']} to={AppRoute.SIGN_IN}>
+                    <Button isPrimary>Get Started</Button>
                 </Link>
             </section>
-            <aside className='home-page__image-section'>
+            <aside className={styles['home-page__image-section']}>
                 <img 
-                    className='home-page__assets-home-image'
+                    className={styles['home-page__assets-home-image']}
                     src={assetsHomeImage} 
                     alt="Assets home image" 
                     loading='lazy'
